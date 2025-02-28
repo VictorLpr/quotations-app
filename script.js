a933d51d2ce9d9f9597e1224499a19e023be45b6
VictorLpr <victorlepron@gmail.com>
2025-02-28 10:45:52 +0100

    etape 5

--- script.js
+++ script.js
@@ -1,10 +1,24 @@
 const form = document.querySelector(".la")
+const quoteList = document.querySelector("#quote-list");
 
 form.addEventListener("submit", (e) => {
     e.preventDefault();
     const quote = document.querySelector("#quote").value;
     const author = document.querySelector("#name").value;
-    console.log(quote);
-    console.log(author)
-
+    addQuote(quote,author);
 })
+
+function addQuote(quote, author) {
+    const text = document.createElement("p");
+    text.classList.add("text");
+    const authorName = document.createElement("p");
+    authorName.classList.add("author")
+    text.innerText = quote;
+    authorName.innerText = author;
+    const div = document.createElement("div");
+    div.classList.add("quote")
+    div.appendChild(text);
+    div.appendChild(authorName);
+    quoteList.appendChild(div)
+
+}
